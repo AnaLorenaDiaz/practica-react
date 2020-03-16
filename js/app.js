6c9c27c513c3d5b690571d06aaab07c75f540c26
AnaLorenaDiaz <analorenadiazperez@gmail.com>
2020-03-15 19:26:38 -0500

    agregar articles con js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function(){
+  
+  var template = document.getElementById("article-items");
+  var articles = document.getElementById("articles");
+
+  for(var i=1; i<=4; i++){
+    var clonado = template.cloneNode(true);
+    clonado.removeAttribute("id");
+    var h3 = clonado.getElementsByTagName("h3")[0];
+    h3.innerHTML = h3.textContent +' '+i;
+    articles.appendChild(clonado);
+  }
+});
