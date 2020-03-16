974d06aa6b15c04fb3c0281dd0d6f4e4bf118cd6
AnaLorenaDiaz <analorenadiazperez@gmail.com>
2020-03-15 22:23:54 -0500

    modelando el contenido del index-article

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-window.addEventListener("load", function(){
+/*window.addEventListener("load", function(){
   
   var template = document.getElementById("article-items");
   var articles = document.getElementById("articles");
 
+  
   for(var i=1; i<=4; i++){
     var clonado = template.cloneNode(true);
     clonado.removeAttribute("id");
@@ -10,4 +11,5 @@ window.addEventListener("load", function(){
     h3.innerHTML = h3.textContent +' '+i;
     articles.appendChild(clonado);
   }
-});
+});*/
+
